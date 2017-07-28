eec553fb7dd7037f79ef2b89486e4f8878c3a555
Alexey Afanasev <avafanasiev@gmail.com>
2017-07-28 15:11:13 +0300

    Refinements

--- src/main/scala/CheckBoardUi.scala
+++ src/main/scala/CheckBoardUi.scala
@@ -1,4 +1,4 @@
-import Main.{Cell, CheckBoard}
+import Main.{Cell, CheckBoard, Size}
 
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
@@ -9,49 +9,81 @@ import org.scalajs.dom.raw.HTMLElement
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom
-import scalatags.JsDom.all._
+import scalatags.JsDom.all.{s, _}
 
 @JSExport
-object CheckBoardUi extends JSApp{
+object CheckBoardUi extends JSApp {
 
   val boxSize = 10
   val cellsCount = boxSize * boxSize
+  val visitor = new WarnsdorfVisitor(CheckBoard(boardSize = boxSize))
+
+  var iterator: Int = 0
 
   @JSExport
   def main(b: html.Body): Unit = {
-    val visitor = new WarnsdorfVisitor(CheckBoard(boardSize = boxSize))
-
     renderContent(b, visitor)
-    dom.window.setInterval(() => {
-      visitor.step()
-      renderContent(b, visitor)
-    }, 50)
+  }
+
+  def startWalking(body: html.Body): Unit = {
+    stopWalking
+    iterator = dom.window.setInterval(
+      () => {
+        val s: Option[(Size, Size)] = visitor.step()
+        if (s.nonEmpty) {
+          renderContent(body, visitor)
+        } else {
+          dom.window.alert(s"done in ${visitor.steps.size} steps")
+          stopWalking
+        }
+      },
+      50
+    )
+  }
+
+  def stopWalking: Unit = {
+    dom.window.clearInterval(iterator)
   }
 
   private def renderContent(body: html.Body, visitor: Visitor) = {
     body.innerHTML = ""
-    type CellView   = JsDom.TypedTag[HTMLElement]
+    type CellView = JsDom.TypedTag[HTMLElement]
 
     def cell(string: JsDom.Modifier, cell: Cell) = {
-      val cls = if (visitor.isVisited(cell)) "col pure-button pure-button-primary" else "col pure-button"
-      div(`class` := cls, string, onclick := { () => visitor.startWalking(cell) })
+      val cls =
+        if (visitor.isVisited(cell)) "col pure-button pure-button-primary"
+        else "col pure-button"
+      div(`class` := cls, string, onclick := { () =>
+        visitor.startWalking(cell)
+        startWalking(body)
+      })
     }
 
     val filler: IndexedSeq[CellView] =
-        (0 until boxSize).flatMap { x =>
-          (0 until boxSize).map { y =>
-            cell(s"($x, $y)", (x, y))
+      (0 to boxSize).flatMap { x =>
+        (0 to boxSize).map { y =>
+          if (x > 0 && y > 0) {
+            cell("", (x - 1, y - 1))
+          } else {
+            if (x > 0) {
+              div(x - 1, `class` := "col")
+            } else if (y > 0) {
+              div(y - 1, `class` := "col")
+            } else div("", `class` := "col")
           }
         }
+      }
 
     body.appendChild(
       div(
+        h4("Pawns!"),
+        h6(i("Refresh anytime to start all over again")),
         `class` := "container",
         div(
-          `class` := "grid-10",
+          `class` := "grid-11",
           filler
         ),
-        p("version: 3")
+        p("version: 4")
       ).render
     )
   }
